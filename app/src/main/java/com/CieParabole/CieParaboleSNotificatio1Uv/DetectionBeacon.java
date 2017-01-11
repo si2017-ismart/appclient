482522e288d76549bba2439c17d1a1e6f43f13de
volcyAnnick <volcy.annick@gmail.com>
2017-01-11 18:42:31 +0100

    CallWebService

--- app/src/main/java/com/CieParabole/CieParaboleSNotificatio1Uv/DetectionBeacon.java
+++ app/src/main/java/com/CieParabole/CieParaboleSNotificatio1Uv/DetectionBeacon.java
@@ -17,16 +17,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 
@@ -35,7 +38,7 @@ public class DetectionBeacon extends AppCompatActivity implements BeaconConsumer
     public static final String TAG = "BeaconsEverywhere";
     private BeaconManager beaconManager;
     private int notificationID = 0;
-
+    private String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +64,12 @@ public class DetectionBeacon extends AppCompatActivity implements BeaconConsumer
     public void onBeaconServiceConnect() {
         final Region region = new Region("myBeaons",null, null, null);
         setContentView(R.layout.activty_detection_find);
+        Button button = (Button) findViewById(R.id.buttonOUI);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                callWebService();
+            }
+        });
         beaconManager.setMonitorNotifier(new MonitorNotifier() {
             @Override
             public void didEnterRegion(Region region) {
@@ -94,8 +103,9 @@ public class DetectionBeacon extends AppCompatActivity implements BeaconConsumer
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
                 for (Beacon oneBeacon : beacons) {
                     Log.d(TAG, "distance: " + oneBeacon.getDistance() + " id:" + oneBeacon.getId1() + "/" + oneBeacon.getId2() + "/" + oneBeacon.getId3());
+                    id = oneBeacon.getId1().toString() + oneBeacon.getId2().toString() + oneBeacon.getId3().toString();
                     if (notificationID == 0) {
-                                addNotification();
+                        addNotification();
                     }
 
                 }
@@ -128,5 +138,25 @@ public class DetectionBeacon extends AppCompatActivity implements BeaconConsumer
 
 
     }
+
+    private void callWebService(){
+        WebService service = new WebService();
+        ArrayList<String> list = service.getAllBeacons();
+        if(checkbeacon(list)){
+            Toast.makeText(getApplicationContext(), "CHECKED", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean checkbeacon(ArrayList<String> list){
+
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).equals(id)){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
 
 }
